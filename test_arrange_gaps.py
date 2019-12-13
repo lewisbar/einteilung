4c9703c26005b35f04396f63fe5899a7f6615843
Lennart Wisbar <lewisbar.git@gmail.com>
2019-12-13 13:35:43 +0100

    Added two more tests for the deficiancy function

--- test_arrange_gaps.py
+++ test_arrange_gaps.py
@@ -9,12 +9,17 @@ def mock_people(amount):
         Person('Person 4', job='Job 1', frequency=1, unavailable=[]),
         Person('Person 5', job='Job 2', frequency=1, unavailable=[]),
         Person('Person 6', job='Job 3', frequency=1, unavailable=[]),
-        Person('Person 7', job='Job 1', frequency=1, unavailable=[]),
-        Person('Person 8', job='Job 2', frequency=1, unavailable=[]),
-        Person('Person 9', job='Job 3', frequency=1, unavailable=[]),
-        Person('Person 10', job='Job 1', frequency=1, unavailable=[]),
-        Person('Person 11', job='Job 2', frequency=1, unavailable=[]),
-        Person('Person 12', job='Job 3', frequency=1, unavailable=[]),
+    ]
+    return people[:amount]
+
+def mock_people_unavailable(amount):
+    people = [
+        Person('Person 1', job='Job 1', frequency=1, unavailable=[0,1,2]),
+        Person('Person 2', job='Job 2', frequency=1, unavailable=[0,1,2]),
+        Person('Person 3', job='Job 3', frequency=1, unavailable=[0,1,2]),
+        Person('Person 4', job='Job 1', frequency=1, unavailable=[0,1,2]),
+        Person('Person 5', job='Job 2', frequency=1, unavailable=[0,1,2]),
+        Person('Person 6', job='Job 3', frequency=1, unavailable=[0,1,2]),
     ]
     return people[:amount]
 
@@ -30,3 +35,10 @@ def test_deficiancy_negative_2():
     '''3 events, 5 people with frequency 1, makes a deficiancy of -2'''
     assert gaps.deficiancy(mock_people(5), 3) == -2
     
+def test_deficiancy_unavailable():
+    '''People are all available as often as their frequencies'''
+    assert gaps.deficiancy(mock_people_unavailable(5), 8) == 3
+
+def test_deficiancy_unavailable_overload():
+    '''People are available less often than their frequencies. The deficiancy function should ignore availability. There are other functions for that.'''
+    assert gaps.deficiancy(mock_people_unavailable(5), 5) == 0
